cc710eaeef2ee0b3f6d03c04a7bde61af899c9ae
kevinnail <fumalicious@gmail.com>
2023-01-19 12:26:16 -0800

    skeleton code added, basic display working

---
+++ src/components/Controls/Controls.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Select from '../Select/Select';
+export default function Controls() {
+  return (
+    <div>
+      <Select />
+    </div>
+  );
+}
---
+++ src/components/Main/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Controls from '../Controls/Controls';
+
+export default function Main() {
+  return (
+    <main>
+      <Controls />
+    </main>
+  );
+}
